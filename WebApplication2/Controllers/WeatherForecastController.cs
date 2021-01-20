35cafe6a96e5b7476f4737c1b25b22144f7a7dd6
sarad <saradaprasad1998@gmail.com>
2021-01-20 13:41:59 +0530

    Changed Contrller Variable

--- WebApplication2/Controllers/WeatherForecastController.cs
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -26,12 +26,12 @@ namespace WebApplication2.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
+            var range = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = range.Next(-20, 55),
+                Summary = Summaries[range.Next(Summaries.Length)]
             })
             .ToArray();
         }
